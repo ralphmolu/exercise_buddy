e70861f0a5389a9739dbbb8c8fb4a614615e6fb8
MajorBriggs <158084320+danrcross@users.noreply.github.com>
2024-03-14 19:57:33 -0400

    added script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
-var ytAPIKeyRalph = "AIzaSyDN2x7IWfitTATr3ByxdWJnBLycKq_T19k";
+var ytAPIKeyDan = 'AIzaSyB9d1Cst7FLimdflVD7dDzQFe6k09qyzsE'
+var searchQuery = 'elvis'
+var ytAPI = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=' + searchQuery + '&key=AIzaSyB9d1Cst7FLimdflVD7dDzQFe6k09qyzsE'
+var ytLink = 'https://www.youtube.com/watch?v='
+
+var exNinApiKey= '38bf2ea7dbmsh996e21bb45906dbp14c699jsn856b582ec0ba'
+var muscle= 'neck'
+var exNinApi= 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=' + muscle
+var exNinApiHost= 'exercises-by-api-ninjas.p.rapidapi.com'
+
+// Info on search queries here: https://developers.google.com/youtube/v3/docs/search/list#usage. click on '</>' icon by 'list (by keyword)
+
+function parseVidIds(data) {
+    data.items.forEach(function (video) {
+        var videoId= video.id.videoId
+        var videoTitle= video.snippet.title
+        if (video.id.kind === 'youtube#video') {
+            console.log(videoTitle + ": " + (ytLink + videoId))
+        } else {
+            return
+        }
+    })
+}
+// Fetch the YT API data
+fetch(ytAPI)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        parseVidIds(data)
+    })
+
+// Fetch the Exercise API data
+fetch(exNinApi,{
+    headers: {
+        'X-RapidAPI-Key': exNinApiKey,
+        'X-RapidAPI-Host': exNinApiHost
+    }})
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+    })
+
