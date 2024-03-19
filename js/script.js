a6a7f80a00c14a4456af994b4ce71402ba71b8e0
Ralph Molu <rmolu11@gmail.com>
2024-03-18 21:56:39 -0400

    created a method to update the API based on the user input and an eventListener on the Find Exercise button

--- js/script.js
+++ js/script.js
@@ -5,9 +5,10 @@ var ytAPI = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxRe
 var ytLink = 'https://www.youtube.com/watch?v='
 
 var exNinApiKey = '38bf2ea7dbmsh996e21bb45906dbp14c699jsn856b582ec0ba'
-var muscle = 'neck'
-var exNinApi = 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=' + muscle
+// var muscle = 'neck'
+var exNinApi = 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises'
 var exNinApiHost = 'exercises-by-api-ninjas.p.rapidapi.com'
+var userChoice = [];
 
 // Info on search queries here: https://developers.google.com/youtube/v3/docs/search/list#usage. click on '</>' icon by 'list (by keyword)
 
@@ -42,10 +43,10 @@ fetch(exNinApi, {
     .then(function (response) {
         return response.json();
     })
-    .then(function (data) {        
+    .then(function (data) {
         console.log(data)
         // TEMPORARY: Using the following two lines to test functionality of exercise list generator
-        exList=data
+        exList = data
         genExList()
     })
 
@@ -61,16 +62,23 @@ $(document).ready(function () {
     })
 
     //event listener on drop-down item to populate main drop down with clicked item
-    $(".dropdown-item").click(function(){
+    $(".dropdown-item").click(function () {
         var dropdownItemText = $(this).text();
         $(this).closest(".dropdown").removeClass("is-active").find("button span").eq(0).text(dropdownItemText);
     })
 
     //collapse dropdown(s) when clicking away from them
-    $(document).click(function(event){
-        if (!$(event.target).closest(".dropdown").length){
+    $(document).click(function (event) {
+        if (!$(event.target).closest(".dropdown").length) {
             $(".dropdown").removeClass("is-active");
         }
+    })
+
+    //eventlistener for find exercises button
+    $("#find-ex-btn").click(function(){
+        updateExNinAPIUrl();
+
+        // window.location.href = "./Exercises.html"
     })
 
 })
@@ -78,18 +86,52 @@ $(document).ready(function () {
 // [feature/gen-ex-list] start
 // Creating code to generate a list of 10 exercises based on user selection
 
-var resultsListEl= $('#results-list')
+var resultsListEl = $('#results-list')
 
 // This var will hold the 10 matches based on user criteria. At the moment, the matching functionality has not been created.
 var exList
 
 
 function genExList() {
-exList.forEach(function(result){
-    // Text of each list item/button will be the title of the exercise. 
-    var resultButtonEl= $('<button>').text(result.name).attr('data-exercise',result).addClass('button is-link m-2')
-    resultsListEl.append(resultButtonEl)
-})
+    exList.forEach(function (result) {
+        // Text of each list item/button will be the title of the exercise. 
+        var resultButtonEl = $('<button>').text(result.name).attr('data-exercise', result).addClass('button is-link m-2')
+        resultsListEl.append(resultButtonEl)
+    })
 }
 
 // [feature/gen-ex-list] end
+
+//update API based on user choices
+function updateExNinAPIUrl() {
+    var baseURL = `https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises`;
+    userChoice = []; // empty array that will store user choices
+
+    var exType = "";
+    var muscle = "";
+    var difficulty = "";
+
+    //check the text in the main/default dropdown box and store it in variables
+    if ($("#ex-main").text() !== 'Choose exercise type here') {
+        exType = $("#ex-main").text().toLowerCase();
+        userChoice.push('type='+ exType);
+    }
+    if ($("#muscle-main").text() !== 'Choose muscle group here') {
+        muscle = $("#muscle-main").text().toLowerCase();
+        userChoice.push('muscle='+ muscle);
+    }
+    if ($("#difficulty-main").text() !== 'Choose difficulty here') {
+        difficulty = $("#difficulty-main").text().toLowerCase();
+        userChoice.push('difficulty='+ difficulty);
+    }
+
+    var exNinApi;
+    if (userChoice.length > 0){
+        exNinApi = baseURL + "?" + userChoice.join('&');
+    } else {
+        exNinApi = baseURL;
+    }
+
+    console.log(exNinApi);
+
+}
